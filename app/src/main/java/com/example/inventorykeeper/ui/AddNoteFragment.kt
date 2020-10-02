f6858db9c980ed80956fd256f918fc0f505486e1
aryanarora180 <aryan.arora180@gmail.com>
2020-10-03 01:25:29 +0530

    Switched to BaseFragment format and using KotlinX view bindings instead of data binding

--- app/src/main/java/com/example/inventorykeeper/ui/AddNoteFragment.kt
+++ app/src/main/java/com/example/inventorykeeper/ui/AddNoteFragment.kt
@@ -1,46 +1,34 @@
 package com.example.inventorykeeper.ui
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import androidx.fragment.app.activityViewModels
 import com.example.inventorykeeper.R
-import com.example.inventorykeeper.databinding.FragmentAddNoteBinding
 import com.example.inventorykeeper.helper.Note
+import kotlinx.android.synthetic.main.fragment_add_note.*
 import java.util.*
 
-class AddNoteFragment : Fragment() {
+class AddNoteFragment : Fragment(R.layout.fragment_add_note) {
 
-    private lateinit var databaseViewModel: DatabaseViewModel
+    private val viewModel by activityViewModels<DatabaseViewModel>()
 
-    private lateinit var binding: FragmentAddNoteBinding
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-    override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
-    ): View? {
-        databaseViewModel = ViewModelProvider(this).get(DatabaseViewModel::class.java)
-        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_add_note, container, false)
-
-        binding.addNoteButton.setOnClickListener {
+        add_note_button.setOnClickListener {
             var title = "Untitled"
             var body = "No body"
-            if(!binding.titleEdit.text.isNullOrBlank())
-                title = binding.titleEdit.text.toString()
-            if(!binding.bodyEdit.text.isNullOrBlank())
-                body = binding.bodyEdit.text.toString()
-            databaseViewModel.insert(Note(
+            if(!title_edit.text.isNullOrBlank())
+                title = title_edit.text.toString()
+            if(!body_edit.text.isNullOrBlank())
+                body = body_edit.text.toString()
+            viewModel.insert(Note(
                 0,
                 title,
                 body,
                 Date().time
             ))
         }
-
-        return binding.root
     }
 }
