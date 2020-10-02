ca5cc4da496f5c3d79662902de880786c5d6517d
aryanarora180 <aryan.arora180@gmail.com>
2020-10-03 01:31:28 +0530

    Fixed Adapter implementation to be inline with recent best practices

--- app/src/main/java/com/example/inventorykeeper/ui/DashboardFragment.kt
+++ app/src/main/java/com/example/inventorykeeper/ui/DashboardFragment.kt
@@ -5,7 +5,6 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.inventorykeeper.R
 import com.example.inventorykeeper.helper.NoteAdapter
 import kotlinx.android.synthetic.main.fragment_dashboard.*
@@ -14,16 +13,15 @@ class DashboardFragment : Fragment(R.layout.fragment_dashboard) {
 
     private val viewModel by activityViewModels<DatabaseViewModel>()
 
+    private val adapter = NoteAdapter()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val adapter = NoteAdapter(requireContext())
         notes_recycler.adapter = adapter
-        notes_recycler.layoutManager = LinearLayoutManager(requireContext())
-
         viewModel.allNotes.observe(viewLifecycleOwner, Observer {notes ->
             notes?.let {
-                adapter.setNotes(notes)
+                adapter.notes = notes
             }
         })
     }
